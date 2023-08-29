f58589bf7c9cc37a5dfcf546c58b74975a4036f7
Ivan Smagin <smagin.op@gmail.com>
2023-08-30 00:06:40 +0300

    First commit

---
+++ src/app/components/footer/footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.css"
+
+function Footer(){
+    return(
+        <div className="footer">
+            <h1>Всем спасибо!</h1>
+            <h5>2023</h5>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/app/components/header/Header.tsx
@@ -0,0 +1,10 @@
+import './header.css'
+
+
+export default function Header() {
+    return (
+        <div className='header'>
+          <h1>Заголовок главной страницы века!</h1>
+        </div>
+    )
+  }
