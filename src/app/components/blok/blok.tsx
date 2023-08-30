4ef440e44a1e535d196a88606c2692a75c9fb872
Ivan Smagin <smagin.op@gmail.com>
2023-08-30 23:26:48 +0300

    Add img

---
+++ src/app/components/blok/blok.tsx
@@ -0,0 +1,10 @@
+import "./blok.css"
+
+export default function Blok(){
+    return(
+        <div className="wrapper">
+            <img src="https://i.gadgets360cdn.com/large/nfs-payback-pc_1509702597620.jpg" alt="payback" />
+        </div>
+        
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Header from "./components/header/Header"
 import Footer from "./components/footer/footer"
+import Blok from "./components/blok/blok"
 
 export default function Home() {
   return (
     <>
     <Header/>
+    <Blok/>
     <Footer/>
     </>
   )
